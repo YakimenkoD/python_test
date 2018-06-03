ed2ee6b3280a24694da7ce4c53ea29da05185987
YakimenkoD <33657657+YakimenkoD@users.noreply.github.com>
2018-06-03 17:04:44 +0300

    little DRY opt

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 
 b_day = date(1996, 2, 29) 
-i_day = date(2010, 3, 13)
+i_day = date(2018, 3, 13)
 
 
 
@@ -14,7 +14,7 @@ def passport_validation(birth,issued):
 			first_day_valid = birth.replace(year=birth.year + 14, month=birth.month+1, day=1)
 		last_day_valid  = birth.replace(year=birth.year + 20, month= birth.month+1)
 
-		return first_day_valid<=issued<=last_day_valid if date.today()>=issued else False
+		result = first_day_valid<=issued<=last_day_valid 
 
 	elif age in range(20,46):
 		try:
@@ -23,7 +23,7 @@ def passport_validation(birth,issued):
 			first_day_valid = birth.replace(year=birth.year + 20, month=birth.month+1, day=1)
 
 		last_day_valid  = birth.replace(year=birth.year + 45, month= birth.month+1)
-		return first_day_valid <= issued <= last_day_valid if date.today()>=issued else False
+		result = first_day_valid <= issued <= last_day_valid
 	
 	elif age >=45:
 		try:
@@ -31,10 +31,11 @@ def passport_validation(birth,issued):
 		except ValueError:
 			first_day_valid = birth.replace(year=birth.year + 45, month=birth.month+1, day=1)
 
-		return first_day_valid<=issued if date.today()>=issued else False
+		result = first_day_valid<=issued 
 
 	else:
-		return False
+		result = False
+	return result if date.today()>=issued else False
 
 print(passport_validation(b_day,i_day))
 
