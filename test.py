dc49e3c18325eac7c6a35cd1f4e54f57afa9291b
YakimenkoD <33657657+YakimenkoD@users.noreply.github.com>
2018-06-03 15:54:13 +0300

    monkey patched leap-year error

--- test.py
+++ test.py
@@ -5,7 +5,10 @@ i_day = date(2014, 3, 13)
 
 def calculate_age(born):
     today = date.today()
-    birthday = born.replace(year=today.year)
+    try: 
+        birthday = born.replace(year=today.year)
+    except ValueError:
+        birthday = born.replace(year=today.year, month=born.month+1, day=1)
     if birthday > today:
         return today.year - born.year - 1
     else:
@@ -15,17 +18,35 @@ def passport_validation(birth,issued):
 	age = calculate_age(b_day)
 
 	if age in range(14,21):
-		first_day_valid = birth.replace(year=birth.year + 14)
-		last_day_valid  = birth.replace(year=birth.year + 20, month= birth.month+1)
+		try:
+			first_day_valid = birth.replace(year=birth.year + 14)
+		except ValueError:
+			first_day_valid = birth.replace(year=birth.year + 14, month=birth.month+1, day=1)
+		
+		try:
+			last_day_valid  = birth.replace(year=birth.year + 20, month= birth.month+1)
+		except ValueError:
+			last_day_valid  = birth.replace(year=birth.year + 20, month= birth.month+2,day=1)
+
 		return print(first_day_valid<=issued<=last_day_valid)
 
 	elif age in range(20,46):
-		first_day_valid = birth.replace(year=birth.year + 20)
-		last_day_valid  = birth.replace(year=birth.year + 45, month= birth.month+1)
-		return print(first_day_valid<=issued<=last_day_valid)
-
+		try:
+			first_day_valid = birth.replace(year=birth.year + 20)
+		except ValueError:
+			first_day_valid = birth.replace(year=birth.year + 20, month=birth.month+1, day=1)
+		
+		try:
+			last_day_valid  = birth.replace(year=birth.year + 45, month= birth.month+1)
+		except ValueError:
+			last_day_valid  = birth.replace(year=birth.year + 45, month= birth.month+2,day=1)
+	
 	elif age >=45:
-		first_day_valid = birth.replace(year=birth.year + 45)
+		try:
+			first_day_valid = birth.replace(year=birth.year + 45)
+		except ValueError:
+			first_day_valid = birth.replace(year=birth.year + 45, month=birth.month+1, day=1)
+
 		return print(first_day_valid<=issued)
 
 	else:
